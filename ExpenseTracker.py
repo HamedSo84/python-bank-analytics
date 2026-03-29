7f171ebbc88c807fe79d0be8aed19428ab54e15f
hamed <hamedsoleimani89@gmail.com>
2026-03-29 12:32:36 -0700

    v 2.1 add datatime add class Transaction

--- ExpenseTracker.py
+++ ExpenseTracker.py
@@ -1,6 +1,22 @@
 import os
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import json
+import datetime
+
+class Transaction:
+    def __init__(self, t_type, amount, balance):
+        self.type = t_type
+        self.amount = amount
+        self.balance = balance
+        self.time = datetime.now().strftime("%Y-%m-%d")
+
+    def to_dict(self):
+        return {
+            "type": self.type,
+            "amount": self.amount,
+            "balance": self.balance,
+            "time": self.time
+        }
 
 class BankAccount:
     def __init__(self, account_name):
@@ -29,12 +45,7 @@ class BankAccount:
             except (json.JSONDecodeError, FileNotFoundError):
                 data = []
 
-        new_transaction = {
-            "type": transaction_type,
-            "amount": amount,
-            "balance": self.balance
-        }
-        data.append(new_transaction)
+        data.append(Transaction.to_dict())
 
         with open(self.file_name, "w", encoding="utf-8") as file:
             json.dump(data, file, indent=4)
@@ -56,50 +67,66 @@ class BankAccount:
         else:
             return "Invalid command"
 
-def show_charts():
-    if not os.path.exists():
-        return "No transactions found! Please add some first"
+    def show_charts(self):
+        if not os.path.exists(self.file_name):
+            return "No transactions found! Please add some first"
 
-    with open(FILE_NAME, "r", encoding="utf-8") as file:
-        try:
-            data = json.load(file)
-        except json.JSONDecodeError:
-            return "File is empty or corrupted"
+        with open(self.file_name, "r", encoding="utf-8") as file:
+            try:
+                data = json.load(file)
+            except json.JSONDecodeError:
+                return "File is empty or corrupted"
 
-    deposits = [0]
-    withdrawals = [0]
-    balance = [0]
+        deposits = [0]
+        withdrawals = [0]
+        balance = [0]
+        dayes = []
 
-    for item in data:
-        balance.append(item["balance"])
-        if item["type"] == "deposit":
-            deposits.append(item["amount"])
-        elif item["type"] == "withdraw":
-            withdrawals.append(item["amount"])
+        for item in data:
+            day = item["time"].split("-")[2]
+            dayes.append(int(day))
+            balance.append(item["balance"])
+            if item["type"] == "deposit":
+                deposits.append(item["amount"])
+                withdrawals.append(0) 
+            elif item["type"] == "withdraw":
+                withdrawals.append(item["amount"])
+                deposits.append(0) 
+        x_transactions = range(len(deposits))
 
-    x_depo = range(len(deposits))
-    x_with = range(len(withdrawals))
-    x_balance = range(len(balance))
+        plt.figure(1)
+        plt.plot(x_transactions, deposits, label="Deposits", color="green", marker="o")
+        plt.plot(x_transactions, withdrawals, label="Withdrawals", color="red", marker="o")
+        plt.title(f"Financial Report - {self.name}")
+        plt.xlabel("Number of Transactions")
+        plt.ylabel("Transaction Amount")
+        plt.grid(True)
+        plt.legend()
 
-    plt.figure(1)
-    plt.plot(x_depo, deposits, label="Deposits", color="green", marker="o")
-    plt.plot(x_with, withdrawals, label="Withdrawals", color="red", marker="o")
-    plt.title("Financial Report")
-    plt.xlabel("Number of Transactions")
-    plt.ylabel("Transaction Amount")
-    plt.grid(True)
-    plt.legend()
+        plt.figure(2)
+        plt.plot(dayes, balance, label="Account_balance", color="blue", marker="o")
+        plt.title(f"Account Balance History - {self.name}")
+        plt.xlabel("Number of Transactions")
+        plt.ylabel("Transaction Amount")
+        plt.grid(True)
+        plt.legend()
 
-    plt.figure(2)
-    plt.plot(x_balance, balance, label="Account_balance", color="blue", marker="o")
-    plt.title("Account Balance History")
-    plt.xlabel("Number of Transactions")
-    plt.ylabel("Transaction Amount")
-    plt.grid(True)
-    plt.legend()
+        plt.figure(3)
+        total_depo = sum(deposits)
+        total_with = sum(withdrawals)
+        categories = ['Total Deposits', 'Total Withdrawals']
+        values = [total_depo, total_with]
+            
+        plt.bar(categories, values, color=['green', 'red'], label="Total Amount")
+            
+        plt.title(f"Total Analysis (Bar Chart) - {self.name}")
+        plt.xlabel("Transaction Category")
+        plt.ylabel("Amount")
+        plt.grid(axis='y', linestyle='--', alpha=0.7) 
+        plt.legend()
 
-    plt.show()
-
+        plt.show()
+        
 if __name__ == "__main__":
     
     my_wallet = BankAccount("Ali")
@@ -107,5 +134,5 @@ if __name__ == "__main__":
     print(my_wallet.add_transaction("deposit", 1500))
     print(my_wallet.add_transaction("withdraw", 500))
     print(my_wallet.add_transaction("withdraw", 50000))
-    
+    print(my_wallet.balance)
     #my_wallet.show_charts()
